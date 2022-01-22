ca7a0e430232dc1b3b7b4ac09bd3cccafb034821
omar-chaar <53823379+omar-chaar@users.noreply.github.com>
2022-01-22 20:06:37 +0200

    Implemented the array on the start

--- js/script.js
+++ js/script.js
@@ -4,17 +4,18 @@ import {SelectionSort, InsertionSort, ShellSort} from "./scripts/Sort.js"
 //Functions loaded when page loads
 
 sideMenu()
-
+let displayArray = createRandomArray(10);
 //Functions
 
 function createRandomArray(size){
-    returnArray = new Array(size);
-    for(i in returnArray){
-        randomNumber = parseInt(Math.random() * 100);
+    let returnArray = new Array(size);
+    for(let i = 0; i < returnArray.length; i++){
+        let randomNumber = parseInt(1 + Math.random() * 30);
         while(returnArray.includes(randomNumber)){
-            randomNumber = parseInt(Math.random() * 100);
+            randomNumber = parseInt(1 + Math.random() * 30);
         }
-        returnArray[i] = randomNumber;
+        returnArray[i] = randomNumber;       
     }
     return returnArray;
-}
+}                                       
+
