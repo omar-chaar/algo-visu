01eac1cb8f51c117f0a7f027de2e8bbc2e5344d9
omar-chaar <53823379+omar-chaar@users.noreply.github.com>
2022-01-22 19:07:01 +0200

    Added createRandomArray function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 import sideMenu from "./template/menu.js";
 
 sideMenu()
+
+
+//Functions
+
+
+function createRandomArray(size){
+    returnArray = new Array(size);
+    for(i in returnArray){
+        randomNumber = parseInt(Math.random() * 100);
+        while(returnArray.includes(randomNumber)){
+            randomNumber = parseInt(Math.random() * 100);
+        }
+        returnArray[i] = randomNumber;
+    }
+    return returnArray;
+}
