682114c4c343443bd3b88cd4207d455462e7ac77
omar-chaar <53823379+omar-chaar@users.noreply.github.com>
2022-01-31 22:17:49 +0200

    Added BubbleSort algorithm (back-end only)

--- js/scripts/Sort.js
+++ js/scripts/Sort.js
@@ -171,3 +171,11 @@ export class MergeSort extends Sort {
 		})
 	}
 }
+export class BubbleSort extends Sort{
+	sort(array){
+		for(let i = 0; i < array.length-1; i++)
+			for(let j = 0; j < array.length - i - 1; j++)
+				if(this.less(array[j+1],array[j]))
+					this.exch(array,j+1,j)
+	}
+}
